8a610dd56eb540a026d524300932b0bbe8f60c49
Masato Aramaki <GitHub>
2025-11-08 14:06:44 +0900

    chore: remove カタカナファイル コード.js

--- コード.js
+++
@@ -1,9 +0,0 @@
-function doGet() {
-  return HtmlService.createHtmlOutputFromFile('index');
-}
-
-function addRecord(data) {
-  var ss = SpreadsheetApp.openById('1bTRSe5l7RTMk1taHNtYaAUMcFBEIwGUf6Yz0icPtp2M');
-  var sheet = ss.getSheetByName('施設リスト');
-  sheet.appendRow([data.name, data.address, data.phone, data.contact, data.notes]);
-}
